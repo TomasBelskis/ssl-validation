25a75d4c61a1fede4a49a96c9b637ae6c5de49cd
TomasBelskis <tomas.belskis@icloud.com>
2020-05-10 15:04:27 +0100

    Fixed the issue with validation of dates

--- ssl-validation.js
+++ ssl-validation.js
@@ -6,17 +6,14 @@ var hosts = ['letsencrypt.org','google.com','apple.com','theverge.com', 'expired
 function check_validity(valid_to){
 	const currentDate = new Date(Date.now());
 	const oneDay = 24 * 60 * 60 * 1000;  
-	console.log(`Valid to: ${valid_to}`);
 	let parsedDate = Date.parse(valid_to);
 	let expiryDate = new Date(parsedDate)
 
 
 	if (Date.parse(currentDate) < parsedDate)
 	{
-	 return Math.round(Math.abs((expiryDate - currentDate) / oneDay));
+	 	return Math.round(Math.abs((expiryDate - currentDate) / oneDay));
 	}else if (Date.parse(currentDate) > parsedDate){
-		// console.log('Current dates epoch: ' + Date.parse(currentDate))
-		// console.log('ExpiryDate: ' + expiryDate);
 		return Math.round((expiryDate - currentDate) / oneDay);
 	}
 }
@@ -56,7 +53,7 @@ function fetchCertInfo(hostname){
 					loc : location,
 					sans : certificate.subjectaltname,
 					issued : valid_from,
-					until: valid_to,
+					valid_until: valid_to,
 					expiring_in : days_until_expiry
 				};
 			 res(cert_info);
@@ -77,11 +74,14 @@ async function storeAllCertInfo(hosts){
 
 async function notify_about_expiring_certs(certDataArr){
 	certDataArr.forEach(certInfo => {
-		if (check_validity(certInfo.expiring_in) >= 1){
+		if (certInfo.expiring_in >= 1){
+			console.log('\n ------------------------------------------------------');
 			console.log(`The certificate for ${ certInfo.hostname } is valid!`);
 			console.log(`Cert expires in ${ certInfo.expiring_in }`);
+			console.log(`Valid until ${ certInfo.valid_until }`)
 
-		}else if (check_validity(certInfo.expiring_in) <= 0 ){
+		}else if (certInfo.expiring_in <= 0 ){
+			console.log('\n ------------------------------------------------------');
 			console.log(`The certificate for ${ certInfo.hostname }has expired!`);
 			console.log(`Days since expiry ${ certInfo.expiring_in }.`);
 			console.log(`Expired in ${ certInfo.until }`);
