5869b6aabc813a7d15ddcb15fff6fbd8f46b4be2
TomasBelskis <tomas.belskis@icloud.com>
2020-05-10 15:09:28 +0100

    Merging with master

--- ssl-validation.js
+++ ssl-validation.js
@@ -1,25 +1,93 @@
 const sslCertificate = require('get-ssl-certificate')
 
-sslCertificate.get('google.com').then(function (certificate) {
-  console.log(certificate)
-  // certificate is a JavaScript object
+var hosts = ['letsencrypt.org','google.com','apple.com','theverge.com', 'expired.badssl.com']
 
-  console.log(certificate.issuer)
-  // { C: 'GB',
-  //   ST: 'Greater Manchester',
-  //   L: 'Salford',
-  //   O: 'COMODO CA Limited',
-  //   CN: 'COMODO RSA Domain Validation Secure Server CA' }
+// Computes the validity of the cert.
+function check_validity(valid_to){
+	const currentDate = new Date(Date.now());
+	const oneDay = 24 * 60 * 60 * 1000;  
+	let parsedDate = Date.parse(valid_to);
+	let expiryDate = new Date(parsedDate)
 
-  console.log(certificate.valid_from)
-  // 'Aug  14 00:00:00 2017 GMT'
 
-  console.log(certificate.valid_to)
-  // 'Nov 20 23:59:59 2019 GMT'
+	if (Date.parse(currentDate) < parsedDate)
+	{
+	 	return Math.round(Math.abs((expiryDate - currentDate) / oneDay));
+	}else if (Date.parse(currentDate) > parsedDate){
+		return Math.round((expiryDate - currentDate) / oneDay);
+	}
+}
 
-  // If there was a certificate.raw attribute, then you can access certificate.pemEncoded
-  console.log(certificate.pemEncoded)
-  // -----BEGIN CERTIFICATE-----
-  // ...
-  // -----END CERTIFICATE-----
+function isObjEmpty(obj) {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
+// Information required
+// Hostname
+// Valid from
+// Valid until
+// Days until expiry
+// Cert Issuer
+// SAN
+// Location
+// return a Json formated result
+function fetchCertInfo(hostname){
+		return new Promise ((res, rej) => {
+			sslCertificate.get(hostname).then(function (certificate) {
+
+				let valid_from = certificate.valid_from;
+				let valid_to = certificate.valid_to;
+				let days_until_expiry = check_validity(valid_to);
+				let string = JSON.stringify(certificate.issuer);
+				let objectValue = JSON.parse(string);
+				let cert_issuer = objectValue['O'];
+				let location = objectValue['L'];
+				let cert_info = {};
+			
+				cert_info = {
+					hostname : hostname,
+					issuer : cert_issuer,
+					loc : location,
+					sans : certificate.subjectaltname,
+					issued : valid_from,
+					valid_until: valid_to,
+					expiring_in : days_until_expiry
+				};
+			 res(cert_info);
+			});
+		});
+}
+
+async function storeAllCertInfo(hosts){
+		let all_certs = [];	 
+			for (const hostname of hosts) { 
+			   let certInfo = await fetchCertInfo(hostname);
+			   all_certs.push(certInfo);
+			   }
+	return new Promise((res, rec) => {
+		res(all_certs);
+	});
+}
+
+async function notify_about_expiring_certs(certDataArr){
+	certDataArr.forEach(certInfo => {
+		if (certInfo.expiring_in >= 1){
+			console.log('\n ------------------------------------------------------');
+			console.log(`The certificate for ${ certInfo.hostname } is valid!`);
+			console.log(`Cert expires in ${ certInfo.expiring_in }`);
+			console.log(`Valid until ${ certInfo.valid_until }`)
+
+		}else if (certInfo.expiring_in <= 0 ){
+			console.log('\n ------------------------------------------------------');
+			console.log(`The certificate for ${ certInfo.hostname }has expired!`);
+			console.log(`Days since expiry ${ certInfo.expiring_in }.`);
+			console.log(`Expired in ${ certInfo.until }`);
+		}	
+	});
+}
+storeAllCertInfo(hosts).then((certDataArr) => {
+	notify_about_expiring_certs(certDataArr);	
 });
